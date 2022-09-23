5a9757b252c55fbb45a27b11d95d0a70bf28b539
shinyZu <shinysirichandra1141@gmail.com>
2022-09-23 18:08:42 +0530

    Created Top Stories & CardTag component

---
+++ az_news-frontend/src/components/00_common/CardTag/CardTag.js
@@ -0,0 +1,45 @@
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import Chip from "@mui/material/Chip";
+
+import styles from "./CardTag.module.css";
+
+function CardTag(props) {
+  return (
+    <>
+      <Grid
+        container
+        item
+        xl={12}
+        lg={12}
+        md={12}
+        sm={12}
+        xs={12}
+        className={styles.card__tag__container}
+        direction="column"
+        justifyContent="flex-end"
+      >
+        <Chip
+          label={props.category}
+          size="small"
+          style={{
+            width: "fit-content",
+            color: "white",
+            border: `1px solid ${props.chip_color}`,
+            backgroundColor: `${props.chip_color}`,
+            marginBottom: "2%",
+            borderRadius: "5px",
+          }}
+        />
+        <Typography variant="h5" className={styles.tag_text}>
+          {props.headline}
+        </Typography>
+        <Typography variant="h7" className={styles.tag_text}>
+          {props.date}
+        </Typography>
+      </Grid>
+    </>
+  );
+}
+
+export default CardTag;
--- az_news-frontend/src/pages/HomePage/HomePage.js
+++ az_news-frontend/src/pages/HomePage/HomePage.js
@@ -1,14 +1,125 @@
+import Grid from "@mui/material/Grid";
+
 import React from "react";
 import Header from "../../components/Home/Header/Header";
 import NavBar from "../../components/Home/NavBar/NavBar";
 
-import { withStyles } from "@mui/styles";
+import b_img from "../../assets/images/business/business1.jpeg";
+
+import styles from "./TopStory.module.css";
+import CardTag from "../../components/00_common/CardTag/CardTag";
 
 function HomePage() {
   return (
     <>
       <Header />
       <NavBar />
+
+      <Grid
+        container
+        xl={12}
+        lg={12}
+        md={12}
+        sm={12}
+        xs={12}
+        className={styles.container__1}
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        <Grid
+          container
+          item
+          xl={5.9}
+          lg={5.9}
+          md={5.9}
+          sm={5.9}
+          xs={5.9}
+          className={styles.container__left}
+        >
+          {/* <img id={1} className={styles.card__img} /> */}
+          <CardTag
+            category="Business"
+            headline="Headline"
+            date="Jan 01"
+            chip_color="#2980b9"
+          />
+        </Grid>
+        <Grid
+          container
+          // item
+          xl={5.9}
+          lg={5.9}
+          md={5.9}
+          sm={5.9}
+          xs={5.9}
+          className={styles.container__right}
+          alignContent="space-between"
+        >
+          <Grid
+            container
+            item
+            xl={12}
+            lg={12}
+            md={12}
+            sm={12}
+            xs={12}
+            className={styles.container__top}
+          >
+            <CardTag
+              category="Tech"
+              headline="Headline"
+              date="Jan 01"
+              chip_color="#8e44ad"
+            />
+          </Grid>
+          <Grid
+            container
+            item
+            xl={12}
+            lg={12}
+            md={12}
+            sm={12}
+            xs={12}
+            className={styles.container__bottom}
+            justifyContent="space-between"
+          >
+            <Grid
+              container
+              item
+              xl={5.9}
+              lg={5.9}
+              md={5.9}
+              sm={5.9}
+              xs={5.9}
+              className={styles.container__bottom_left}
+            >
+              <CardTag
+                category="Entertainment"
+                headline="Headline"
+                date="Jan 01"
+                chip_color="#16a085"
+              />
+            </Grid>
+            <Grid
+              container
+              item
+              xl={5.9}
+              lg={5.9}
+              md={5.9}
+              sm={5.9}
+              xs={5.9}
+              className={styles.container__bottom_right}
+            >
+              <CardTag
+                category="Sports"
+                headline="Headline"
+                date="Jan 01"
+                chip_color="#f1c40f"
+              />
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
     </>
   );
 }
